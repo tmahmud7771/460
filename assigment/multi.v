0da61d884f2d0645dc396e90c9a8c44b6e51aa99
Tausif <tmahmud44@gmail.com>
2025-01-02 16:46:38 +0600

    fixes

--- assigment/multi.v
+++ assigment/multi.v
@@ -7,28 +7,23 @@ module sequence_detector(
     output reg z
 );
 
-    // State registers for each sequence
-    reg [1:0] state_w;    // For 1010 sequence (needs fewer states in Mealy)
-    reg [1:0] state_x;    // For 111 sequence
-    reg [1:0] state_y;    // For 10 sequence
-    reg z_w, z_x, z_y;    // Individual outputs
+    reg [1:0] state_w;
+    reg [1:0] state_x;
+    reg [1:0] state_y;
+    reg z_w, z_x, z_y;
 
-    // State parameters for w (1010) - Needs fewer states in Mealy
-    parameter W_S0 = 2'b00;  // Initial
-    parameter W_S1 = 2'b01;  // Saw 1
-    parameter W_S2 = 2'b10;  // Saw 10
-    parameter W_S3 = 2'b11;  // Saw 101
+    parameter W_S0 = 2'b00;
+    parameter W_S1 = 2'b01;
+    parameter W_S2 = 2'b10;
+    parameter W_S3 = 2'b11;
 
-    // State parameters for x (111)
-    parameter X_S0 = 2'b00;  // Initial
-    parameter X_S1 = 2'b01;  // Saw 1
-    parameter X_S2 = 2'b10;  // Saw 11
+    parameter X_S0 = 2'b00;
+    parameter X_S1 = 2'b01;
+    parameter X_S2 = 2'b10;
 
-    // State parameters for y (10)
-    parameter Y_S0 = 2'b00;  // Initial
-    parameter Y_S1 = 2'b01;  // Saw 1
+    parameter Y_S0 = 2'b00;
+    parameter Y_S1 = 2'b01;
 
-    // Sequential block for state transitions
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             state_w <= W_S0;
@@ -36,7 +31,6 @@ module sequence_detector(
             state_y <= Y_S0;
         end
         else begin
-            // For sequence 1010
             case (state_w)
                 W_S0: state_w <= w ? W_S1 : W_S0;
                 W_S1: state_w <= w ? W_S1 : W_S2;
@@ -45,7 +39,6 @@ module sequence_detector(
                 default: state_w <= W_S0;
             endcase
 
-            // For sequence 111
             case (state_x)
                 X_S0: state_x <= x ? X_S1 : X_S0;
                 X_S1: state_x <= x ? X_S2 : X_S0;
@@ -53,7 +46,6 @@ module sequence_detector(
                 default: state_x <= X_S0;
             endcase
 
-            // For sequence 10
             case (state_y)
                 Y_S0: state_y <= y ? Y_S1 : Y_S0;
                 Y_S1: state_y <= y ? Y_S1 : Y_S0;
@@ -62,19 +54,12 @@ module sequence_detector(
         end
     end
 
-    // Combinational output logic - Mealy style
     always @(*) begin
-        // For 1010 sequence
-        z_w = (state_w == W_S3) && !w;  // Output 1 when in state 101 and input is 0
-
-        // For 111 sequence
-        z_x = (state_x == X_S2) && x;   // Output 1 when in state 11 and input is 1
-
-        // For 10 sequence
-        z_y = (state_y == Y_S1) && !y;  // Output 1 when in state 1 and input is 0
+        z_w = (state_w == W_S3) && !w;
+        z_x = (state_x == X_S2) && x;
+        z_y = (state_y == Y_S1) && !y;
     end
 
-    // Combine all outputs
     always @(negedge clk) begin
         z <= z_w | z_x | z_y;
     end
